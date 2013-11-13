a8960c192c2191512ed2df786491b65f1c4d3016
OpenShift guest <venom-lp@yandex.ru>
2013-11-13 02:43:26 -0500

    comtfdvdfdds

--- MyApp.js
+++ MyApp.js
@@ -26,3 +26,29 @@ io.sockets.on('connection', function (socket) {
       console.log(data);
    });
 });
+
+var mongo = require('mongoskin');
+var conn = mongo.db('mongodb://venom-lp:spider11@dharma.mongohq.com:10001/nodeJSDataBase');
+
+conn.collection('collectionName').update(
+{
+   user:"userToUpdate"
+},
+{
+   user:"userToUpdate",
+   someData: "someNewData"
+},
+{
+   upsert:true
+});
+
+conn.collection('collectionName').findOne(
+{
+   user:"userToFind"
+},
+function(err, doc)
+{
+   if (err) { /* something is wrong */ }
+   if (doc) { var foundData = doc.someData; }
+});
+
